40be3727232ea3854e64465bcd314c146e5427f6
helenzhaochen <zhaochen.helen@yahoo.com>
2018-07-16 10:12:49 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,101 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-div', { preload: preload, create: create, update: update });
+
+function preload() {
+
+    game.load.image('sky', 'assets/sky.png');
+    game.load.image('ground', 'assets/platform.png');
+    game.load.image('star', 'assets/star.png');
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    
+}
+
+//Declare variables outside of functions.
+
+var keys;
+var players;
+var ground;
+var platform;
+
+function create() {
+
+    game.add.sprite(0,0,"sky");
+    game.add.sprite(300,300,'star');
+    game.add.sprite(300,400,'star');
+    game.add.sprite(300,500,'star');
+
+    // Ground
+    platforms = game.add.group();
+    
+    //Create the ground
+    ground = platforms.create(0,game.world.height - 40,'ground');
+
+    // Double size of the ground
+    ground.scale.setTo(2, 2);
+    
+    //Enable collision
+    game.physics.arcade.enable(ground);
+    
+    //Stops the ground from falling
+    ground.body.immovable = true;
+    
+    //Add dude
+    player = game.add.sprite(32, game.world.height - 150, 'dude');
+    
+    //Enabling dude to move
+    game.physics.arcade.enable(player);
+    
+    //Player Physics properties
+    player.body.gravity.y = 300;
+    
+    //Controls
+    keys = game.input.keyboard.createCursorKeys();
+    
+    //Add animations to player
+    player.animations.add('left', [0, 1, 2, 3,], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
+    
+    // Add ledge
+    var ledge1 = platforms.create(400, 400, 'ground');
+    var ledge2 = platforms.create(-150, 250, 'ground');
+    
+   // Enable physics on platforms
+    game.physics.arcade.enable(platforms);
+    
+    // Prevent the ledges from moving
+    ledge1.body.immovable = true;
+    ledge2.body.immovable = true;
+}
+
+function update() {
+    //Collision between the player and ground
+    game.physics.arcade.collide(player, ground);
+    
+    // Check for collisions between the player and all platforms
+    game.physics.arcade.collide(player, platforms);
+    
+    if (keys.left.isDown) {
+        //  Move to the left
+        player.body.velocity.x = -150;
+        player.animations.play('left');
+    }
+    else if (keys.right.isDown) {
+        //  Move to the right
+        player.body.velocity.x = 150;
+        player.animations.play('right');
+    }
+    else {
+        //Stop
+        player.body.velocity.x = 0;
+        
+        //Stand Still
+        player.animations.stop();
+        
+        // Reset animation frame
+        player.frame = 4;
+    }
+    
+    if (keys.up.isDown && player.body.touching.down) 
+    {
+        player.body.velocity.y = -350;
+    }
+}
